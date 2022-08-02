6d613d51e3c80470417999afcbeb40daefac165d
Soledad Adorno <gabriela.juj@gmail.com>
2022-08-01 22:52:30 -0300

    primer commit, fueron creadas las carpeta header, logo-arg y social. First commit, I created header, logo-arg and social components (folders)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoArgComponent } from './components/logo-arg/logo-arg.component';
+import { SocialComponent } from './components/social/social.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoArgComponent,
+    SocialComponent
   ],
   imports: [
     BrowserModule
