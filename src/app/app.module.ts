31a60d7271eec72552a5b82d4ab4916d0d409d6b
Soledad Adorno <gabriela.juj@gmail.com>
2022-08-02 23:52:50 -0300

    se creo el componente boton-login y se agrego bootstrap a el angular.json

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoArgComponent } from './components/logo-arg/logo-arg.component';
 import { SocialComponent } from './components/social/social.component';
+import { BotonLoginComponent } from './components/boton-login/boton-login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     LogoArgComponent,
-    SocialComponent
+    SocialComponent,
+    BotonLoginComponent
   ],
   imports: [
     BrowserModule
