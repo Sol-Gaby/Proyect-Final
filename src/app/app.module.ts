adcbd74291a74f2263bdf20d8678cb7791a5523f
Soledad Adorno <gabriela.juj@gmail.com>
2022-08-04 16:18:16 -0300

    se independizo la barra del geader, la saque fuera del recuadro blanco

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
-import { LogoArgComponent } from './components/logo-arg/logo-arg.component';
-import { SocialComponent } from './components/social/social.component';
-import { BotonLoginComponent } from './components/boton-login/boton-login.component';
 import { AboutComponent } from './components/about/about.component';
 import { ExperienceComponent } from './components/experience/experience.component';
 import { EducationComponent } from './components/education/education.component';
@@ -13,21 +10,20 @@ import { SkillsComponent } from './components/skills/skills.component';
 import { ProgressBarComponent } from './components/progress-bar/progress-bar.component';
 import { ProyectsComponent } from './components/proyects/proyects.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { BarraIconosComponent } from './components/barra-iconos/barra-iconos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    LogoArgComponent,
-    SocialComponent,
-    BotonLoginComponent,
     AboutComponent,
     ExperienceComponent,
     EducationComponent,
     SkillsComponent,
     ProgressBarComponent,
     ProyectsComponent,
-    FooterComponent
+    FooterComponent,
+    BarraIconosComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/barra-iconos/barra-iconos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-barra-iconos',
+  templateUrl: './barra-iconos.component.html',
+  styleUrls: ['./barra-iconos.component.css']
+})
+export class BarraIconosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
